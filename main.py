df57e6f6bfe0a0aa5bed6820266ce33f3701f25c
tejashyamehta <tejashya@gmail.com>
2025-01-31 17:18:10 +0530

    Countdown updates

--- main.py
+++ main.py
@@ -17,9 +17,19 @@ reps = 0
 def start_timer():
     global reps
     reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
+    if reps % 8 == 0:
+        count_down(long_break_sec)
 
-    count_down(9)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+
+    else:
+        count_down(work_sec)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
